5b1fa4339d135c940fd496c8a2414933359e9ed1
Kelvin Jones <kevinjones4453@gmail.com>
2024-04-16 21:37:05 -0400

    [Upgrade]: step 4 - upgrade to Angular 11

--- src/app/taskpane/task-grid/task-grid.component.spec.ts
+++ src/app/taskpane/task-grid/task-grid.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TaskGridComponent } from './task-grid.component';
 
@@ -6,7 +6,7 @@ describe('TaskGridComponent', () => {
   let component: TaskGridComponent;
   let fixture: ComponentFixture<TaskGridComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TaskGridComponent ]
     })
--- src/app/taskpane/taskpane.component.spec.ts
+++ src/app/taskpane/taskpane.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TaskpaneComponent } from './taskpane.component';
 
@@ -6,7 +6,7 @@ describe('TaskpaneComponent', () => {
   let component: TaskpaneComponent;
   let fixture: ComponentFixture<TaskpaneComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TaskpaneComponent ]
     })
